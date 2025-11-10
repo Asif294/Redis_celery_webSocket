0c94064c1a0e118c78dbac1c18fef97ad6863518
Asif294 <asifurrahman677294@gmail.com>
2025-11-10 21:37:14 +0600

    first commit

---
+++ product/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.core.cache import cache
+from .models import Products
+
+class ProductListView(APIView):
+    def get(self,request):
+        product=cache.get('all_products')
+        if not product:
+            print('faceing from database')
+            queryset=Products.objects.all()
+            serializer=ProductSerializer(queryset,many=True)
+            product=serializer.data
+            cache.set('all_products',product,timeout=60)
+        else:
+            print("faceing from redis cache")
+        return Response(product)
+    
+
+
