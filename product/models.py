45e84973579f97318cb5b52514663e39f0b6b900
Asif294 <asifurrahman677294@gmail.com>
2025-11-18 16:45:35 +0600

    model

--- product/models.py
+++ product/models.py
@@ -9,3 +9,14 @@ class Products(models.Model):
         return self.name
 
 
+class Course(models.Model):
+    title =models.CharField(max_length=150)
+    price =models.DecimalField(max_digits=8 ,decimal_places=2)
+    description=models.TextField()
+    category=models.CharField(max_length=100)
+    is_featured=models.BooleanField(default=True)
+    is_paid=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
